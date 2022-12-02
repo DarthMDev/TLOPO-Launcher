dfa161ed00837237ccea1894e071319af0bb66a8
DarthNihilus1 <mgracer48@yahoo.com>
2022-12-01 21:20:42 -0500

    Fix the start_steamdeck script

--- start_steamdeck.sh
+++ start_steamdeck.sh
@@ -1,4 +1,4 @@
 # add username and password arguments
 export username="$1"
 export password="$2"
-python3 main.py --username $username$ --password $password$
+python3 main.py --username $username --password $password
